0fe737fe65745f580de90563b4af7a03507f3e7d
sandeeppreetam <33050344+sandeeppreetam@users.noreply.github.com>
2023-06-14 16:18:59 +0000

    connection encryption

--- database.py
+++ database.py
@@ -1,17 +1,19 @@
 from sqlalchemy import create_engine, text
+import os
 
 # Create an engine
-connection_string = 'mysql+mysqlconnector://up0zrppdknffht0xiwnj:pscale_pw_iI37EzH1qKEL7HFOHc22Xsty9pBBmhkFkdR0BL210sW@aws.connect.psdb.cloud/desicharades'
+connection_string = os.getenv("connection_string")
 engine = create_engine(connection_string, echo=True)
-conn = engine.connect()
 
-# Execute SQL code
-sql = text("SELECT * FROM films")
-result = conn.execute(sql)
+def execute(engine, sql):
+    with engine.connect() as conn:
+        result = conn.execute(text(sql))
+        columns = result.keys()
+        rows = result.fetchall()
+        output = [dict(zip(columns, row)) for row in rows]
+        return output
 
-# Process the results
-for row in result:
-    print(row)
+films = execute(engine, "select * from films")
+print(films[0])
 
-# Close the connection
-conn.close()
+
